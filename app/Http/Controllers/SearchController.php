8cc0e800dc18d6e88a6c5e4d8bfa2e64b261bf0e
Dmytro Liakin (psd2html) <dmytro.liakin@w3.co>
2016-05-17 11:51:24 +0300

    del searchers

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -7,19 +7,17 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
-use Illuminate\Support\Facades\DB;
-use sngrl\SphinxSearch\SphinxSearch;
 
 class SearchController extends Controller
 {
     public function search(Request $request)
     {
-        $sphinx = new SphinxSearch();
-        $Products = Product::whereIn('id', array_column(json_decode(json_encode($sphinx->search($request->srch, 'product')->get()), true), 'id'))
+        $Products = Product::where('title', 'LIKE', '%' . $request->srch . '%')
+            ->orWhere('description', 'LIKE', '%' . $request->srch . '%')
             ->get();
-        $Categories = Category::whereIn('id', array_column(json_decode(json_encode($sphinx->search($request->srch, 'category')->get()), true), 'id'))
+        $Categories = Category::where('title', 'LIKE', '%' . $request->srch . '%')
+            ->orWhere('description', 'LIKE', '%' . $request->srch . '%')
             ->get();
-
         return view('search')
             ->with(['srch' => $request->srch])
             ->with(['Products' => $Products])
